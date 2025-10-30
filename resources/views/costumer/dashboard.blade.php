b54c971aa433cf9040e51140f517a0ac70b45b2f
harutosgirlfriend <dyahayuetikawati@gmail.com>
2025-10-30 23:47:30 +0700

    nambahin

--- resources/views/costumer/dashboard.blade.php
+++ resources/views/costumer/dashboard.blade.php
@@ -7,7 +7,7 @@
 
         <div class="relative z-10  grid lg:grid-cols-2 h-full">
             <div class="card px-30 py-30">
-                <h1 class="text-4xl font-bold text-gray-800">Wawan</h1>
+                <h1 class="text-4xl font-bold  text-[#a01800]">Wawan</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis doloremque illo obcaecati animi in ut
                     tempore modi cum assumenda tempora accusantium perferendis quae incidunt enim, ea aliquid porro. Ipsa,
                     id.</p>
@@ -18,9 +18,10 @@
 
     </div>
     <div class="flex flex-col justify-center items-center ">
-        <h1 class="text-4xl font-bold text-gray-800 mb-10">Ada Apa Saja Disini?</h1>
+        <h1 class="text-4xl font-bold mb-10  text-[#a01800]">Ada Apa Saja Disini?</h1>
         <div class="best-product flex flex-row justify-center gap-10 py-10 best-pro md:w-full">
-            <div class="rounded-md justify-items-center w-70 shadow-xl shadow-[#e4c6ba]/50">
+       
+            <div class="rounded-md bg- justify-items-center w-70 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
 
                 <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
 
@@ -32,10 +33,15 @@
                     <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
                         deleniti tenetur perferendis! Dolores distinctio amet quas accusantium esse! Aliquam tempore ratione
                         labore eius libero eligendi cum molestias velit.</p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
 
                 </ul>
             </div>
-            <div class="rounded-md bg- justify-items-center w-70 shadow-xl shadow-[#e4c6ba]/50">
+          
+       
+            <div class="rounded-md bg- justify-items-center w-70 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
 
                 <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
 
@@ -47,10 +53,15 @@
                     <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
                         deleniti tenetur perferendis! Dolores distinctio amet quas accusantium esse! Aliquam tempore ratione
                         labore eius libero eligendi cum molestias velit.</p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
 
                 </ul>
             </div>
-            <div class="rounded-md bg- justify-items-center  w-70 shadow-xl shadow-[#e4c6ba]/50">
+          
+       
+            <div class="rounded-md bg- justify-items-center w-70 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
 
                 <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
 
@@ -62,21 +73,107 @@
                     <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
                         deleniti tenetur perferendis! Dolores distinctio amet quas accusantium esse! Aliquam tempore ratione
                         labore eius libero eligendi cum molestias velit.</p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
 
                 </ul>
             </div>
-
+          
         </div>
     </div>
     <div
         class="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-2 items-center header-2 lg:h-[400px] w-full bg-cover bg-[#e4c6ba] shadow-xl shadow-[#e4c6ba]/70 rounded-md">
 
-<p class="px-10 py-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dolorem sed iure nemo aliquam illo eveniet nulla animi velit expedita, possimus maiores ipsum reprehenderit, sint, rerum cum consequuntur. Dolor, iure?</p>
+        <p class="px-10 py-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dolorem sed iure nemo aliquam
+            illo eveniet nulla animi velit expedita, possimus maiores ipsum reprehenderit, sint, rerum cum consequuntur.
+            Dolor, iure?</p>
 
- <img src="{{ asset('../images/ayam.png') }}" alt="" class="object-cover rounded px-10 py-3 block mx-auto">
+        <img src="{{ asset('../images/ayam.png') }}" alt="" class="object-cover rounded px-10 py-3 block mx-auto">
 
-<p class="px-10 py-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dolorem sed iure nemo aliquam illo eveniet nulla animi velit expedita, possimus maiores ipsum reprehenderit, sint, rerum cum consequuntur. Dolor, iure?</p>
+        <p class="px-10 py-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dolorem sed iure nemo aliquam
+            illo eveniet nulla animi velit expedita, possimus maiores ipsum reprehenderit, sint, rerum cum consequuntur.
+            Dolor, iure?</p>
     </div>
 
+    <div class="flex flex-col justify-center items-center p-10">
+        <h1 class="text-4xl font-bold text-[#a01800] mb-10">Top 4 Product</h1>
+        <div class="best-product flex flex-row justify-center gap-10 py-10 best-pro md:w-full">
+       
+            <div class="rounded-md bg- justify-items-center w-60 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
 
+                <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
+
+                <ul>
+
+
+
+                    <li class="px-2 py-3 font-semibold text-center">Stok </li>
+                    <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
+                  </p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
+
+                </ul>
+            </div>
+       
+            <div class="rounded-md bg- justify-items-center w-60 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
+
+                <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
+
+                <ul>
+
+
+
+                    <li class="px-2 py-3 font-semibold text-center">Stok </li>
+                    <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
+                      </p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
+
+                </ul>
+            </div>
+          
+       
+            <div class="rounded-md bg- justify-items-center w-60 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
+
+                <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
+
+                <ul>
+
+
+
+                    <li class="px-2 py-3 font-semibold text-center">Stok </li>
+                    <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
+                       </p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
+
+                </ul>
+            </div>
+          
+       
+            <div class="rounded-md bg- justify-items-center w-60 shadow-xl shadow-[#e4c6ba]/50 p-5 hover:scale-110 hover:cursor-pointer">
+
+                <img src="{{ asset('../images/best-product2.jpg') }}" alt="" class="object-cover rounded w-70">
+
+                <ul>
+
+
+
+                    <li class="px-2 py-3 font-semibold text-center">Stok </li>
+                    <p class="py-2 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ullam corrupti
+                       </p>
+                    <button
+                        class="hover:text-[#b25353] hover:bg-[#f0e9e9] px-2 py-3 block bg-[#b25353] text-white font-semibold rounded-md justify-self-center shadow-xl shadow-[#e4c6ba]/50">Beli
+                        Sekarang</button>
+
+                </ul>
+            </div>
+          
+        </div>
+    </div>
 @endsection
